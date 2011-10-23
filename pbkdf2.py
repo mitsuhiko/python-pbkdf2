97852256d5a61262cec792f2a67d7b0bbba99871
Armin Ronacher <armin.ronacher@active-4.com>
2011-10-23 12:21:36 +0300

    Basic speedup for the pbkdf2 module.

--- pbkdf2.py
+++ pbkdf2.py
@@ -42,9 +42,12 @@
 """
 import hmac
 import hashlib
-from math import ceil
-from struct import pack
-from itertools import izip
+from struct import Struct
+from operator import xor
+from itertools import izip, imap, starmap
+
+
+_pack_int = Struct('>I').pack
 
 
 def pbkdf2_hex(data, salt, iterations=1000, keylen=24, hashfunc=None):
@@ -59,15 +62,19 @@ def pbkdf2_bin(data, salt, iterations=1000, keylen=24, hashfunc=None):
     a different hashlib `hashfunc` can be provided.
     """
     hashfunc = hashfunc or hashlib.sha1
-    _pseudorandom = lambda x: hmac.new(data, x, hashfunc).digest()
-    def _produce(block):
-        rv = u = _pseudorandom(salt + pack('>i', block))
+    mac = hmac.new(data, None, hashfunc)
+    def _pseudorandom(x, mac=mac):
+        h = mac.copy()
+        h.update(x)
+        return map(ord, h.digest())
+    buf = []
+    for block in xrange(1, -(-keylen // mac.digest_size) + 1):
+        rv = u = _pseudorandom(salt + _pack_int(block))
         for i in xrange(iterations - 1):
-            u = _pseudorandom(u)
-            rv = ''.join(chr(ord(a) ^ ord(b)) for a, b in izip(rv, u))
-        return rv
-    blocks = int(ceil(float(keylen) / hashfunc().digest_size))
-    return ''.join(map(_produce, xrange(1, blocks + 1)))[:keylen]
+            u = _pseudorandom(''.join(map(chr, u)))
+            rv = starmap(xor, izip(rv, u))
+        buf.extend(rv)
+    return ''.join(map(chr, buf))[:keylen]
 
 
 def test():
