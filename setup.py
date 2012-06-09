81afd7e6e8e8dcecd2946a52da766603d0726394
Armin Ronacher <armin.ronacher@active-4.com>
2012-06-09 14:06:39 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup
+
+readme = open(os.path.join(os.path.dirname(__file__), 'README'), 'r').read()
+
+setup(
+    name='curlish',
+    author='Armin Ronacher',
+    author_email='armin.ronacher@active-4.com',
+    version='1.0',
+    url='http://github.com/mitsuhiko/python-pbkdf2',
+    py_modules=['pbkdf2'],
+    description='A simple and straightforward implementation of pbkdf2.  Also copy/pasteable',
+    long_description=readme,
+    zip_safe=False,
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python'
+    ]
+)
